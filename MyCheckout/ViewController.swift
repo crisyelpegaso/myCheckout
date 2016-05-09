b651eed737f006759a7cd0cf0d67dfb7ef061ce5
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2016-05-09 15:16:48 -0300

    Initial commit

---
+++ MyCheckout/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  MyCheckout
+//
+//  Created by Maria cristina rodriguez on 6/5/16.
+//  Copyright © 2016 MercadoPago. All rights reserved.
+//
+
+import UIKit
+import MercadoPagoSDK
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var startCheckoutButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.startCheckoutButton.addTarget(self, action: #selector(ViewController.startMPCheckout), forControlEvents: .TouchUpInside)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func startMPCheckout(){
+        
+        let checkoutFlow = MPFlowBuilder.startCheckoutViewController("lala") { (payment) in
+            print(payment.toJSONString())
+        }
+        
+        self.presentViewController(checkoutFlow, animated: true) {
+            
+        }
+
+    }
+
+
+}
+
