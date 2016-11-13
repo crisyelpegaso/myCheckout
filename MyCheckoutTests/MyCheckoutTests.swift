50b5a4a507ed123976dc3a5b686a79e5097f14ac
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2016-11-13 20:41:05 -0300

    Add runTests script

--- MyCheckoutTests/MyCheckoutTests.swift
+++ MyCheckoutTests/MyCheckoutTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-@testable import MyCheckout
+//@testable import MyCheckout
 
 class MyCheckoutTests: XCTestCase {
     
@@ -28,9 +28,9 @@ class MyCheckoutTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        self.measure({ 
+			
+		})
     }
     
 }
