a4bb3216d7e39214b34317664655bec356c6e919
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2016-11-12 23:12:29 -0300

    Adding test

---
+++ MyCheckoutTests/TetstTravis.swift
@@ -0,0 +1,19 @@
+//
+//  TetstTravis.swift
+//  MyCheckout
+//
+//  Created by Maria cristina rodriguez on 11/12/16.
+//  Copyright © 2016 MercadoPago. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TetstTravis: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(true)
+    }
+}
