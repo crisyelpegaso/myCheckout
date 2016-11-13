4e4a70e929a6949c6e8e32a4ec397e6c6a8ec421
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2016-11-13 20:27:46 -0300

    travis updated

--- MyCheckout/ViewController.swift
+++ MyCheckout/ViewController.swift
@@ -10,32 +10,32 @@ import UIKit
 //import MercadoPagoSDK
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var startCheckoutButton: UIButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // self.startCheckoutButton.addTarget(self, action: #selector(ViewController.startMPCheckout), forControlEvents: .TouchUpInside)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func startMPCheckout(){
-        
-      /*  let checkoutFlow = MPFlowBuilder.startCheckoutViewController("lala", callback : { (payment) in
-            print(payment.toJSONString())
-        })
-        
-        self.present(checkoutFlow, animated: true) {
-            
-        }*/
-
-    }
-
-
+	
+	@IBOutlet weak var startCheckoutButton: UIButton!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		// self.startCheckoutButton.addTarget(self, action: #selector(ViewController.startMPCheckout), forControlEvents: .TouchUpInside)
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	func startMPCheckout(){
+		
+		/*  let checkoutFlow = MPFlowBuilder.startCheckoutViewController("lala", callback : { (payment) in
+		print(payment.toJSONString())
+		})
+		
+		self.present(checkoutFlow, animated: true) {
+		
+		}*/
+		
+	}
+	
+	
 }
 
